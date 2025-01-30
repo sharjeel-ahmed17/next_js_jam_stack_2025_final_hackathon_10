00ae3daa1c33ee1e17c9901dc89e7d38f62222f5
sharjeel-ahemd17 <sharjeelahmed2@gmail.com>
2025-01-30 18:04:10 +0500

    create mini component

---
+++ src/components/reuseable/Heading.tsx
@@ -0,0 +1,9 @@
+
+
+const Heading = ({title} : {title : string}) => {
+  return (
+    <div>{title}</div>
+  )
+}
+
+export default Heading
---
+++ src/components/reuseable/SubHeading.tsx
@@ -0,0 +1,9 @@
+
+
+const SubHeading = ({title} : {title : string}) => {
+  return (
+    <div>{title}</div>
+  )
+}
+
+export default SubHeading
