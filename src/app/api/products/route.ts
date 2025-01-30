9846e72a2f093684daa5e2cb67d5ca45b41ddcd2
sharjeel-ahemd17 <sharjeelahmed2@gmail.com>
2025-01-30 16:15:27 +0500

    update

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,9 +1,15 @@
+import { GET_ALL_PRODUCTS } from "@/lib/queries";
+import { client } from "@/sanity/lib/client";
 import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
+        const response = client.fetch(`*[_type == "products"]`);
         return NextResponse.json({
-            data : "some things",
+            data : response,
+            err : false ,
+            message : "data fetch successfully" 
+
         })
     } catch (error) {
         console.log(error);
